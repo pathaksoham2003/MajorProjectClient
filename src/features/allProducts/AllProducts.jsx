9d7f62a175529d7e09d0601694671b2b46190d2a
pathaksoham2003 <120130000+pathaksoham2003@users.noreply.github.com>
2023-09-30 11:04:26 +0530

    30/09/2023 Updated Svg

--- src/features/allProducts/AllProducts.jsx
+++ src/features/allProducts/AllProducts.jsx
@@ -2,6 +2,43 @@ import React, { useState } from 'react'
 import styles from "./AllProducts.module.css";
 import Card from '../allFeatureComponents/Card'
 const AllProducts = () => {
+  const [data , setData ]  = useState([{
+    name:"Marshal Headsetdsjavjdfjbdjfbjdfjsbnjdfnsbjkfdhjbvhjdbjvdjfbjdfjhbhdfbdjfbjkdfsjvbfhjdbvdjfbfbvhdfbvdfjvbbfdjfdvbdfjb",
+    price:2000,
+    rating:4,
+    imageUrl:"https://i.pinimg.com/550x/49/44/55/4944553088c4c7009154e4093738f549.jpg"
+  },{
+    name:"Marshal Headsetdsjavjdfjbdjfbjdfjsbnjdfnsbjkfdhjbvhjdbjvdjfbjdfjhbhdfbdjfbjkdfsjvbfhjdbvdjfbfbvhdfbvdfjvbbfdjfdvbdfjb",
+    price:2000,
+    rating:4,
+    imageUrl:"https://i.pinimg.com/550x/49/44/55/4944553088c4c7009154e4093738f549.jpg"
+  },{
+    name:"Marshal Headsetdsjavjdfjbdjfbjdfjsbnjdfnsbjkfdhjbvhjdbjvdjfbjdfjhbhdfbdjfbjkdfsjvbfhjdbvdjfbfbvhdfbvdfjvbbfdjfdvbdfjb",
+    price:2000,
+    rating:4,
+    imageUrl:"https://i.pinimg.com/550x/49/44/55/4944553088c4c7009154e4093738f549.jpg"
+  },{
+    name:"Marshal Headsetdsjavjdfjbdjfbjdfjsbnjdfnsbjkfdhjbvhjdbjvdjfbjdfjhbhdfbdjfbjkdfsjvbfhjdbvdjfbfbvhdfbvdfjvbbfdjfdvbdfjb",
+    price:2000,
+    rating:4,
+    imageUrl:"https://i.pinimg.com/550x/49/44/55/4944553088c4c7009154e4093738f549.jpg"
+  },{
+    name:"Marshal Headsetdsjavjdfjbdjfbjdfjsbnjdfnsbjkfdhjbvhjdbjvdjfbjdfjhbhdfbdjfbjkdfsjvbfhjdbvdjfbfbvhdfbvdfjvbbfdjfdvbdfjb",
+    price:2000,
+    rating:4,
+    imageUrl:"https://i.pinimg.com/550x/49/44/55/4944553088c4c7009154e4093738f549.jpg"
+  },{
+    name:"Marshal Headsetdsjavjdfjbdjfbjdfjsbnjdfnsbjkfdhjbvhjdbjvdjfbjdfjhbhdfbdjfbjkdfsjvbfhjdbvdjfbfbvhdfbvdfjvbbfdjfdvbdfjb",
+    price:2000,
+    rating:4,
+    imageUrl:"https://i.pinimg.com/550x/49/44/55/4944553088c4c7009154e4093738f549.jpg"
+  },{
+    name:"Marshal Headsetdsjavjdfjbdjfbjdfjsbnjdfnsbjkfdhjbvhjdbjvdjfbjdfjhbhdfbdjfbjkdfsjvbfhjdbvdjfbfbvhdfbvdfjvbbfdjfdvbdfjb",
+    price:2000,
+    rating:4,
+    imageUrl:"https://i.pinimg.com/550x/49/44/55/4944553088c4c7009154e4093738f549.jpg"
+  }])
+  
   return (
     <div className={styles.container}>
       {data.map((data)=> (
--- src/pages/components/NavBar.jsx
+++ src/pages/components/NavBar.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import styles from "./NavBar.module.css";
-import Cart from "../../assets/cart.svg";
-import Heart from "../../assets/heart.svg";
+import Cart from "../../assets/cart-icon.svg";
+import Heart from "../../assets/favorite-svgrepo-com.svg";
 import HamburgerMenu from "../../assets/hamburgerMenu.svg";
 import Cross from "../../assets/close.svg";
 import DarkMode from "./DarkMode.jsx";
@@ -11,20 +11,38 @@ const NavBar = () => {
 
   return (
     <div className={`${styles.navbar}`}>
-      <div className={`${styles.brand}`}>ECommerce<DarkMode/>
+      <div className={`${styles.brand}`}>
+        ECommerce
+        <DarkMode />
       </div>
       <div className={`${styles.toggle}`}>
         <div className={`${styles.icons}`}>
-          <Link to={`favorite/1`}><img src={Heart} /></Link>
-          <Link to={`cart`}
-          ><img src={Cart} /></Link>
+          <Link to={`favorite/1`}>
+            <img className={styles.heart} src={Heart} />
+          </Link>
+          <Link to={`cart`}>
+            <img className={styles.cart} src={Cart} />
+          </Link>
         </div>
-        <div className={styles.hamburger} onClick={()=>setIsExpanded(prev=>!prev)}>{isExpanded? <img src={Cross}/>:<img src={HamburgerMenu}/>}</div>
-        <ul className={isExpanded ? `${styles.expandMenu}` : `${styles.menu}` }>
-          <li onClick={()=>setIsExpanded(false)}><Link to="/">Home</Link></li>
-          <li onClick={()=>setIsExpanded(false)}><Link to="/products">Product</Link></li>
-          <li onClick={()=>setIsExpanded(false)}><Link to="/login">Login</Link></li>
-          <li onClick={()=>setIsExpanded(false)}><Link to="/register">Register</Link></li>
+        <div
+          className={styles.hamburger}
+          onClick={() => setIsExpanded((prev) => !prev)}
+        >
+          {isExpanded ? <img src={Cross} /> : <img src={HamburgerMenu} />}
+        </div>
+        <ul className={isExpanded ? `${styles.expandMenu}` : `${styles.menu}`}>
+          <li onClick={() => setIsExpanded(false)}>
+            <Link to="/">Home</Link>
+          </li>
+          <li onClick={() => setIsExpanded(false)}>
+            <Link to="/products">Product</Link>
+          </li>
+          <li onClick={() => setIsExpanded(false)}>
+            <Link to="/login">Login</Link>
+          </li>
+          <li onClick={() => setIsExpanded(false)}>
+            <Link to="/register">Register</Link>
+          </li>
         </ul>
       </div>
     </div>
