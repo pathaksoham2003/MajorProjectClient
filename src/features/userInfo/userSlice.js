55143e36f67ee015ba666942f7d05fec0cab9937
pathaksoham2003 <120130000+pathaksoham2003@users.noreply.github.com>
2023-10-06 02:14:12 +0530

    06/10/2023 Google Signin in process

--- src/features/userInfo/userSlice.js
+++ src/features/userInfo/userSlice.js
@@ -1,5 +1,34 @@
 import {createSlice , createAsyncThunk} from "@reduxjs/toolkit";
+import {createUser,checkUser} from "../../utils/api";
+
+const createUser = createAsyncThunk("User/create",async(userData)=>{
+    const response = await fetch(createUser,{
+        method:"POST",
+        headers:{
+            "Content-Type":"application/json"
+        },
+        body:JSON.stringify(userData)
+    })
+    const data = await response.json();
+})
 
 
+const userSlice = createSlice({
+    name:"User",
+    initialState:{
+        user_id:"",
+        name:"",
+        email:"",
+        google_id:"",
+        picture:"",
+        address:"",
+        error:"",
+        loading:false,
+    },
+    reducers:{
 
+    },
+    extraReducers:(builder)=>{
 
+    }
+})
--- src/utils/api.js
+++ src/utils/api.js
@@ -1,10 +1,10 @@
 const host = import.meta.env.VITE_SERVER_URL;
 // USER ROUTES 
-export const createUser = `${host}api/createUser`;
+export const createUser = `${host}api/user/create`;
 export const checkUser = `${host}api/checkUser`;
 // PRODUCT ROUTES 
-export const getAllProducts = `${host}api/product/getall`;
-export const getParticularProduct = `${host}api/product/get`;
+export const getAllProducts = `${host}api/product/all`;
+export const getSpecificProduct = `${host}api/product/specific`;
 // LOCAL STOREAGE 
 export const TOKEN = localStorage.getItem("TOKEN");
 export const USERID = localStorage.getItem("USERID");
