2a4589797be9d54b19b62c13a900a09e31cf5844
SOHAM PATHAK <120130000+pathaksoham2003@users.noreply.github.com>
2023-10-05 14:41:09 +0530

    05/10/2023 Google AUthentication button

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { GoogleOAuthProvider } from '@react-oauth/google'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <GoogleOAuthProvider clientId="45346336800-osjsbuchfgvsdrj9nmn1ufaea43b1ovp.apps.googleusercontent.com">
     <App />
+    </GoogleOAuthProvider>
   </React.StrictMode>,
 )
--- src/pages/components/GoogleLogin.jsx
+++ src/pages/components/GoogleLogin.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import styles from "./GoogleLogin.module.css";
+import {googleLogout,useGoogleLogin} from "@react-oauth/google";
 
 const GoogleLogin = () => {
+
+
+  const login = () => {
+    useGoogleLogin({
+      onSuccess : () => {},
+      onError : () => {},
+    })
+  }
+
   return (
-    <div>GoogleLogin</div>
+    <div className={styles.container}>
+      <div>
+      <button onClick={login}>Sign In With Google 🚀</button>
+      <hr/>
+      <button onClick ={()=>{}}>GUEST LOGIN</button>
+      </div>
+    </div>
   )
 }
 
