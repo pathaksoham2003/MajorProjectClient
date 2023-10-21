6a768749bdddc76133352b6ef7c53ce162076fce
pathaksoham2003 <120130000+pathaksoham2003@users.noreply.github.com>
2023-10-21 20:19:14 +0530

    api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,11 +1,11 @@
 const host = import.meta.env.VITE_SERVER_URL;
 // USER ROUTES 
-export const createUser = `${host}api/user/create`;
-export const checkUser = `${host}api/user/checkUser`;
-export const getSpecific = `${host}api/user/specific/`;
+export const createUser = `${host}/api/user/create`;
+export const checkUser = `${host}/api/user/checkUser`;
+export const getSpecific = `${host}/api/user/specific/`;
 // PRODUCT ROUTES 
-export const getAllProducts = `${host}api/product/all`;
-export const getSpecificProduct = `${host}api/product/specific`;
+export const getAllProducts = `${host}/api/product/all`;
+export const getSpecificProduct = `${host}/api/product/specific`;
 // LOCAL STOREAGE 
 export const google_id = localStorage.getItem("google_id");
 export const TOKEN = localStorage.getItem("TOKEN");
