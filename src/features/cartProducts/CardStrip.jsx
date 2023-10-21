7c65d3cabe766148f7910ca28b6c421f44250927
pathaksoham2003 <120130000+pathaksoham2003@users.noreply.github.com>
2023-10-21 20:22:00 +0530

    Update CardStrip.jsx

--- src/features/cartProducts/CardStrip.jsx
+++ src/features/cartProducts/CardStrip.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import styles from "../allFeatureComponents/CardStrip.module.css";
 import DarkHeartBreak from "../../assets/heart-break-svgrepo-com.svg?react";
 import { useDispatch } from 'react-redux';
-import { removeFavorite } from './favoriteProductsSlice';
 const CardStrip = ({data}) => {
     const dispatch = useDispatch();
    return (
@@ -14,7 +13,7 @@ const CardStrip = ({data}) => {
             <h3>{data.name}</h3>
             <h3>{data.price}</h3>
             <h3>{data.rating}</h3>
-            <button onClick={()=>{dispatch(removeFavorite(data.product_id))}}>Remove Favorite</button>
+            <button onClick={()=>{}}>Remove Favorite</button>
             <button>Buy Now</button>
         </div>
     </div>
