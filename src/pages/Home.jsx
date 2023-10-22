defd2ad4321055cb918f55daab4ecc9378ef9f8e
pathaksoham2003 <120130000+pathaksoham2003@users.noreply.github.com>
2023-10-22 19:11:58 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,13 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import {getAllProduct} from "../features/allProducts/allProductsSlice";
 
 const Home = () => {
+  const dispatch = useDispatch();
+  useEffect(()=>{
+    dispatch(getAllProduct())
+  },[])
+  
   return (
     <div>Kindly Visit Other Pages This Page is in development</div>
   )
